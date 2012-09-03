d4ff30693d9104dc196f4a4709b92ba92fc1e0c3
The Dod <unclezzzen@gmail.com>
2012-09-04 00:43:44 +0700

    create data/with-ids if needed

--- upload-data.sh
+++ upload-data.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+if [ ! -d data/with-ids ]; then mkdir data/with-ids; fi
 kanso transform add-ids data/example.json data/with-ids/example.json
 kanso upload data/with-ids/example.json "$1"
